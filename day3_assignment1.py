3c95f5258cf58f188770a06711be85ff10aa0c1e
Yaman Birla <68591556+cosmic-weber@users.noreply.github.com>
2020-09-04 18:12:25 +0530

    Delete day3_assignment1.py

--- day3_assignment1.py
+++
@@ -1,9 +0,0 @@
-# program for pilot
-alt = int(input("Enter altitude: "))
-
-if (alt == 1000 or alt < 1000):
-    print('Safe to land')
-elif ( alt > 1000 and alt < 5000):
-    print('Bring down to 1000')
-elif alt > 5000:
-    print('Turn Around')
